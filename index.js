8cb0d10aa407b893e1438f517d2de28730e419c8
Nguyen <tvngiagia@gmail.com>
2019-05-20 14:19:35 +0700

    Test again

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ app.get('/cuoi', function (req, res) {
     res.send("Hello");
 
 });
-app.post('/signup', function (req, res) {
-
-    res.send("Bạn đã tạo tk thành công" + "<br>"+req.body.username +"<br>"+req.body.fullname+"<br>"+req.body.password);
-
+app.get('/haha', function (req, res) {
+    res.send("Hello");
 });
-
+app.post('/signup', function (req, res) {
+    res.send("Bạn đã tạo tk thành công" + "<br>"+req.body.username +"<br>"+req.body.fullname+"<br>"+req.body.password);
+});
 app.listen(port, function () {
     console.log("Hello");
 });
