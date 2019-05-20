bc105d820f3045ead212bab5c898e187180059a4
Nguyen <tvngiagia@gmail.com>
2019-05-20 14:11:02 +0700

    test

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+var bodyParser = require('body-parser');
+const  port = 8888;
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.get('/cuoi', function (req, res) {
+    res.send("Hello");
+
+});
+app.post('/signup', function (req, res) {
+
+    res.send("Bạn đã tạo tk thành công" + "<br>"+req.body.username +"<br>"+req.body.fullname+"<br>"+req.body.password);
+
+});
+
+app.listen(port, function () {
+    console.log("Hello");
+});
+
